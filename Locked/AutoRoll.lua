c22ecfebbda13381ac5d7abcf07d45ff1491f592
justgotripped <97247956+Rippeed@users.noreply.github.com>
2024-04-15 19:19:20 +0000

    fully support weapon & trait, height support WIP

--- Locked/AutoRoll.lua
+++ Locked/AutoRoll.lua
@@ -3,15 +3,31 @@ getgenv().noYenChange = true --// Refrain yen from going out of ur account when 
 
 --// ROLL SETTINGS //--
 getgenv().skipAnimation = true --// Highly recommended to keep it on
-getgenv().rollType = "Trait" --// Choices: "Trait" or "Weapon"
-getgenv().toRoll = {"Clamps"} --// What you want to roll
+getgenv().rollType = "Weapon" --// Choices: "Trait" or "Weapon"
+getgenv().toRoll = {"Shadow"} --// What you want to roll
 
+--// TABLES //--
+local heights = {
+    [1] = "5'3",
+    [2] = "5'4",
+    [3] = "5'5",
+    [4] = "5'6",
+    [5] = "5'7",
+    [6] = "5'8",
+    [7] = "5'9",
+    [8] = "5'10",
+    [9] = "5'11",
+    [10] = "6",
+    [11] = "6'1",
+    [12] = "6'2",
+    [13] = "6'3"
+}
 
 --// FUNCTIONS //--
-local isHookedYen = false
+getgenv().IS_YEN_HOOKED = false
 local function noYenChange()
     local event = game:GetService("ReplicatedStorage").yenchange
-    isHookedYen = true
+    getgenv().IS_YEN_HOOKED = true
     local old;
     old = hookmetamethod(game, "__namecall", function(self, ...)
         local method = getnamecallmethod()
@@ -31,7 +47,7 @@ if getgenv().noYenChange == true then
     noYenChange()
 end
 
-local function getAnimFunc()
+local function getTraitAnim()
     for i, v in pairs(getgc()) do
         if type(v) == 'function' then
             if string.find(debug.info(v, "s"), "NewGacha") and debug.info(v, "n") == "weaponroll"  then
@@ -41,55 +57,147 @@ local function getAnimFunc()
     end
 end
 
-local func = nil
-local function autoRoll()
-    if getgenv().skipAnimation == true then
-        func = getAnimFunc()
-        local old;
-        old = hookfunction(func, function(self, ...)
-            return nil
-        end)
+local function getChangeWeaponFunc()
+    for i,v in pairs(getgc()) do
+        if type(v) == 'function' then
+            if string.find(debug.info(v, "s"), "NewSystem") and debug.info(v, "n") == "changeweapon" then
+                return v
+            end
+        end
     end
+end
 
-    if isHookedYen == false then
-        noYenChange()
+local function getWeaponAnim()
+    for i, v in pairs(getgc()) do
+        if type(v) == 'function' then
+            if string.find(debug.info(v, "s"), "NewSystem") and debug.info(v, "n") == "weaponroll"  then
+                return v
+            end
+        end
     end
+end
 
-    local currentRoll = nil
-    local pathtovalues;
-    if getgenv().rollType == "Trait" then
-        pathtovalues = game.Players.LocalPlayer.Backpack["Trait"]
-    elseif getgenv().rollType == "Weapon" then
-        pathtovalues = game.Players.LocalPlayer.Backpack
+local function getRemote(rolltype)
+    local gamefolder = game:GetService("ReplicatedStorage"):FindFirstChild("rerolls")
+    if rolltype == "Trait" then
+        return gamefolder["traitreroll"]
+    elseif rolltype == "Weapon" then
+        return gamefolder["specreroll"]
+    elseif rolltype == "Height" then
+        return gamefolder["heightreroll"]
+    elseif rolltype == "Face" then
+        return gamefolder["facereroll"]
+    elseif rolltype == "Buff" then
+        return gamefolder["buffreroll"]
+    end
+end
+
+local function getPath(rolltype)
+    local backpack = game.Players.LocalPlayer.Backpack
+    local char = game.Players.LocalPlayer.Character
+
+    if rolltype == "Trait" then
+        return backpack["Trait"]
+    elseif rolltype == "Weapon" then
+        return backpack
+    elseif rolltype == "Height" then
+        return char["HeightValue"]
+    elseif rolltype == "Face" then
+        return char["face"]["Decal"]
+    elseif rolltype == "Buff" then
+        return char["AuraColour"]["Buff"]
     else
         warn("rollType has not been correctly specified, contact the developer if you think this error is a mistake!")
         warn("rollType has not been correctly specified, contact the developer if you think this error is a mistake!")
         warn("rollType has not been correctly specified, contact the developer if you think this error is a mistake!")
-        return
+        print("⛔ Stopping script")
+        return nil
     end
+end
+
+
+local oldweapon;
+local oldtrait;
+local function autoRoll()
+    if getgenv().skipAnimation == true then
+        local func = getTraitAnim()
+        local sfunc = getWeaponAnim()
+        if func == nil or sfunc == nil then 
+            warn("⚠️ Functions fetched are nil! Consider rejoining the game ⚠️")
+            print("⛔ Stopping script")
+            return
+        end
+        oldtrait = hookfunction(func, function(self, ...)
+            return nil
+        end)
+
+        oldweapon = hookfunction(sfunc, function(self, ...)
+            return nil
+        end)
+    end
+
+    if getgenv().IS_YEN_HOOKED == false then
+        noYenChange()
+    end
+
+    local currentRoll = nil
+    local pathtovalues = getPath(getgenv().rollType)
+    if pathtovalues == nil then return end
+
     
     local a;
+    local changefunction = getChangeWeaponFunc()
     task.spawn(function()
-        a = pathtovalues.ChildAdded:Connect(function(inst)
-            currentRoll = inst.Name
-            print(currentRoll)
-        end)
+        if getgenv().rollType == "Trait" or "Weapon" then
+            a = pathtovalues.ChildAdded:Connect(function(inst)
+                currentRoll = inst.Name
+                print(currentRoll)
+                if getgenv().rollType == "Weapon" then
+                    changefunction(inst)
+                end
+            end)
+        elseif getgenv().rollType == "Height" then
+            a = pathtovalues:GetPropertyChangedSignal("Value"):Connect(function()
+                currentRoll = pathtovalues.Value
+                print(heights[currentRoll], currentRoll)
+            end)
+        end
     end)
 
 
-    --// WIP //--
-    local pathtoremote = game:GetService("ReplicatedStorage").rerolls.traitreroll
+    local pathtoremote = getRemote(getgenv().rollType)
     task.spawn(function()
         while task.wait(0.3) do  
-            task.spawn(function()
-                if table.find(getgenv().toRoll, currentRoll) then
-                    print("Successfully rolled; "..currentRoll)
-                    a:Disconnect()
-                    restorefunction(func)
-                    break
-                end
-            end)
+            if table.find(getgenv().toRoll, currentRoll) then
+                print("✅ - Successfully rolled; "..currentRoll)
+                print("If you don't see the roll in the menu, just rejoin the game 🔄")
+                a:Disconnect()
+                restorefunction(oldtrait)
+                restorefunction(oldweapon) 
+                break
+            end
             pathtoremote:FireServer()
         end
     end)
 end
+
+--// Initialization //--
+if getgenv().rollType == "Height" then
+    if string.find(getgenv().toRoll, "'") then
+        for i,v in pairs(heights) do
+            if v == getgenv().toRoll then
+                getgenv().toRoll = i
+            end
+        end
+    elseif getgenv().toRoll == "6" then
+        getgenv().toRoll = 10 --// 10 == 6 foot
+    end
+end
+
+for i = 1, 3 do
+    print("⏳ Starting in..".. i)
+    task.wait(0.5)
+end
+
+autoRoll()
+
